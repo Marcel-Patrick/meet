a6fb93b4dfa1eb531bc293717d0fe69cb89a848d
Marcel-Patrick <schnuffel.hase25@outlook.com>
2022-02-02 00:39:53 +0100

    Write end-to-end tests
    - Write end-to-end tests for the feature: “Filter events by city.”
    - Create another scope inside “EndToEnd.test.js”
    - write end-to-end tests using Puppeteer
    
    Related to Bonus Task 4.5

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 describe("show/hide an event details", () => {
   let browser;
   let page;
-  jest.setTimeout(30000);
+  jest.setTimeout(50000);
   beforeAll(async () => {
     // browser = await puppeteer.launch();
 
@@ -36,5 +36,46 @@ describe("show/hide an event details", () => {
     await page.click(".event .event__detailsButton");
     const eventDetails = await page.$(".event .event__moreDetails");
     expect(eventDetails).toBeNull();
+  });
+});
+
+describe("Filter events by city.", () => {
+  let browser;
+  let page;
+  jest.setTimeout(50000);
+  beforeAll(async () => {
+    // browser = await puppeteer.launch();
+
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("When user hasn’t searched for a city, show upcoming events from all cities", async () => {
+    const countEvents = await page.$$eval(".event", (element) => element.length);
+    expect(countEvents).toBe(2);
+  });
+
+  test("User should see a list of suggestions when they search for a city", async () => {
+    await page.type(".city", "Berlin", { delay: 100 }); //type slower than user
+    const countCities = await page.$$eval(".suggestions li", (element) => element.length);
+    expect(countCities).toBe(2);
+  });
+
+  test("User can select a city from the suggested list", async () => {
+    await page.reload();
+    await page.type(".city", "Berlin", { delay: 100 }); //type slower than user
+    await page.click(".suggestions li");
+    const countEvents = await page.$$eval(".event", (element) => element.length);
+    expect(countEvents).toBe(1);
   });
 });
