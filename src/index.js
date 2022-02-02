4651b680018fbfa4e1d2698324c6b27a09042e11
Marcel-Patrick <schnuffel.hase25@outlook.com>
2022-02-02 14:43:42 +0100

    Add Monitoring with Atatus
    Added Monitoring with Atatus
    Updated index.js
    
    Related to exercise 4.6

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+
+// Monitoring with Atatus
+import * as atatus from "atatus-spa";
+atatus.config("da8b93cae512412aa3880f42ff6029d9").install();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
