cc385f0b8731d0678f44ce1316f85b25448337cc
Marcel-Patrick <90202110+Marcel-Patrick@users.noreply.github.com>
2022-02-08 11:26:56 +0100

    Update Chart Style

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -34,7 +34,7 @@ const renderActiveShape = (props) => {
 
   return (
     <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+      <text fontSize="20px" x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
         {payload.name}
       </text>
       <Sector
@@ -58,13 +58,21 @@ const renderActiveShape = (props) => {
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
       <text
+        fontSize="14px"
         x={ex + (cos >= 0 ? 1 : -1) * 12}
         y={ey}
         textAnchor={textAnchor}
         fill="#333"
       >{`${value} ${addStoEvent}`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-        {`(${(percent * 100).toFixed(2)}%)`}
+      <text
+        fontSize="12px"
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`${(percent * 100).toFixed(2)}%`}
       </text>
     </g>
   );
